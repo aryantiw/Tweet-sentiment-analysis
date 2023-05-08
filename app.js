2f9930773205e85b59937c3da43fe88bc31b5100
aryantiw <aryantiwari543@gmail.com>
2023-05-08 12:09:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@
 //   }
   
 
+//CODE 2
+
 function analyze() {
     const sentence = document.getElementById('input-box').value;
     const resultDiv = document.getElementById('result');
@@ -48,11 +50,40 @@ function analyze() {
     // Return neutral by default if no emotions detected
     return 'Neutral';
   }
+
+
+//CODE 3
+
+
+// function analyzeSentiment(sentence) {
+//     const positiveWords = ["love", "like", "happy", "joy", "excited", "awesome", "fantastic", "great", "amazing", "excellent", "wonderful"];
+//     const negativeWords = ["hate", "dislike", "sad", "unhappy", "angry", "frustrated", "terrible", "awful", "horrible", "bad", "disappointing"];
+//     const neutralWords = ["okay", "fine", "average", "neutral", "indifferent", "boring", "routine", "common", "typical", "regular", "standard"];
+    
+//     const words = sentence.toLowerCase().split(/\W+/);
+//     let positiveCount = 0;
+//     let negativeCount = 0;
+//     let neutralCount = 0;
+    
+//     for (let word of words) {
+//       if (positiveWords.includes(word)) {
+//         positiveCount++;
+//       } else if (negativeWords.includes(word)) {
+//         negativeCount++;
+//       } else if (neutralWords.includes(word)) {
+//         neutralCount++;
+//       }
+//     }
+    
+//     if (positiveCount > negativeCount && positiveCount > neutralCount) {
+//       return "Positive";
+//     } else if (negativeCount > positiveCount && negativeCount > neutralCount) {
+//       return "Negative";
+//     } else {
+//       return "Neutral";
+//     }
+//   }
   
-
-
-
-
 
 
 
